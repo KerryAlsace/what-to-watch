a520b14372e8034bb01f0893ec671efd1cc5c189
KerryAlsace <kerrydougherty@gmail.com>
2016-09-02 15:39:51 -0400

    add show edit and delete and show details controller routes and views details

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,11 +1,13 @@
 class ShowsController < ApplicationController
 
+  ######## SHOW INDEX #########
   get '/shows' do
     @user = current_user
     @current_user_shows = current_user.shows
     erb :'/shows/list_shows'
   end
 
+  ######## CREATE SHOW #########
   get '/shows/new' do
     @user = current_user
     erb :'/shows/create_show'
@@ -18,6 +20,30 @@ class ShowsController < ApplicationController
     redirect '/shows'
   end
 
+  ######## SHOW DETAILS #########
+  get '/shows/:slug' do
+    @show = Show.find(params["slug"])
+    erb :'/shows/show_details'
+  end
 
+  ######## EDIT SHOW #########
+  get '/shows/:slug/edit' do
+    @show = Show.find(params["slug"])
+    erb :'/shows/edit_show'
+  end
+
+  patch '/shows/:slug' do
+    @show = Show.find(params["slug"])
+    @show.update(title: params["title"], genre: params["genre"], length: params["length"])
+    erb :'/shows/show_details'
+  end
+
+  ######## DELETE SHOW #########
+
+  post 'shows/:slug/delete' do
+    @show = Show.find(params["slug"])
+    @show.destroy
+    redirect '/shows'
+  end
 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
   belongs_to :user
+
+  def slug
+    title.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    Show.all.find{ |show| show.slug == slug}
+  end
+
 end
