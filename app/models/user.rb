a76b2169e297130312a591e48deefc5de818ca1d
KerryAlsace <kerrydougherty@gmail.com>
2016-09-02 09:09:44 -0400

    add to user model to get tests to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   has_many :shows
+  validates :display_name, :presence => true
+  validates :username, :presence => true
+  validates :password, :presence => true
 
   has_secure_password
-  
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{ |user| user.slug == slug}
+  end
+
 end
