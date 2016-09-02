209a533dff621ba5e0f347732eca5611e5614bb7
KerryAlsace <kerrydougherty@gmail.com>
2016-09-02 14:33:45 -0400

    create functionality for user's shows to display on /shows page

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,12 +2,20 @@ class ShowsController < ApplicationController
 
   get '/shows' do
     @user = current_user
+    @current_user_shows = current_user.shows
     erb :'/shows/list_shows'
   end
 
   get '/shows/new' do
     @user = current_user
     erb :'/shows/create_show'
+  end
+
+  post '/shows' do
+    @show = Show.create(title: params["title"], genre: params["genre"], length: params["length"])
+    @show.user = current_user
+    @show.save
+    redirect '/shows'
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902124411) do
+ActiveRecord::Schema.define(version: 20160902182725) do
 
   create_table "shows", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160902124411) do
     t.integer  "length"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
