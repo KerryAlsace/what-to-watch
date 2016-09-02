5c2973a72b7ceea7e6a540b83a71db7164f02def
KerryAlsace <kerrydougherty@gmail.com>
2016-09-02 10:27:54 -0400

    add helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,30 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+
+    def login(username, password)
+      @user = User.find_by(username: username)
+      if @user && @user.authenticate(password)
+        session[:user_id] = @user.user_id
+      else
+        redirect to '/login'
+      end
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      # @current_user ||= User.find_by(username: session["username"]) if session["username"]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def logout!
+      session.clear
+    end
+
+  end
+
 end
